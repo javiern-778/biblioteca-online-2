279952ce7f9c1b6907bd02cf3241341aad873ca4
javiern-778 <jcruzespana2@gmail.com>
2025-05-29 19:12:34 -0600

    cambios de nombre

--- backend/server.js
+++ backend/server.js
@@ -10,31 +10,31 @@ app.use('/libros', express.static(__dirname + '/libros'))
 const libros = [
   {
     id: 1,
-    titulo: 'El Quijote',
+    titulo: 'El-Quijote',
     autor: 'Miguel de Cervantes',
-    imagen: 'http://localhost:3000/images/El Quijote.png',
-    archivo: 'http://localhost:3000/libros/El Quijote.pdf'
+    imagen: 'http://localhost:3000/images/El-Quijote.png',
+    archivo: 'http://localhost:3000/libros/El-Quijote.pdf'
   },
   {
     id: 2,
-    titulo: 'Cien Años de Soledad',
+    titulo: 'Cien-Años-de-Soledad',
     autor: 'Gabriel García Márquez',
-    imagen: 'http://localhost:3000/images/Cien Años de Soledad.jpg',
-    archivo: 'http://localhost:3000/libros/Cien Años de Soledad.pdf'
+    imagen: 'http://localhost:3000/images/Cien-Años-de-Soledad.jpg',
+    archivo: 'http://localhost:3000/libros/Cien-Años-de-Soledad.pdf'
   },
   {
     id: 3,
-    titulo: 'Orgullo y prejuicio',
+    titulo: 'Orgullo-y-prejuicio',
     autor: ' Jane Austen',
-    imagen: 'http://localhost:3000/images/Orgullo y prejuicio.jpg',
-    archivo: 'http://localhost:3000/libros/Orgullo y prejuicio.pdf'
+    imagen: 'http://localhost:3000/images/Orgullo-y-prejuicio.jpg',
+    archivo: 'http://localhost:3000/libros/Orgullo-y-prejuicio.pdf'
   },
   {
     id: 4,
     titulo: 'Crimen y castigo',
-    autor: 'Gabriel García Márquez',
-    imagen: 'http://localhost:3000/images/Crimen y castigo.jpg',
-    archivo: 'http://localhost:3000/libros/Crimen y castigo.pdf'
+    autor: 'Fiódor Dostoyevski',
+    imagen: 'http://localhost:3000/images/Crimen-y-castigo.jpg',
+    archivo: 'http://localhost:3000/libros/Crimen-y-castigo.pdf'
   }
 ]
 
