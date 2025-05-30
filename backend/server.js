922a5066e79090e0f97b7f50d678c761e33bb6ec
javiern-778 <jcruzespana2@gmail.com>
2025-05-29 18:45:21 -0600

    agregando libros he imagen

--- backend/server.js
+++ backend/server.js
@@ -12,15 +12,15 @@ const libros = [
     id: 1,
     titulo: 'El Quijote',
     autor: 'Miguel de Cervantes',
-    imagen: 'http://localhost:3000/images/portada1.jpg',
-    archivo: 'http://localhost:3000/libros/libro1.pdf'
+    imagen: 'http://localhost:3000/images/El Quijote.png',
+    archivo: 'http://localhost:3000/libros/El Quijote.pdf'
   },
   {
     id: 2,
     titulo: 'Cien Años de Soledad',
     autor: 'Gabriel García Márquez',
-    imagen: 'http://localhost:3000/images/portada2.jpg',
-    archivo: 'http://localhost:3000/libros/libro2.pdf'
+    imagen: 'http://localhost:3000/images/Cien Años de Soledad.jpg',
+    archivo: 'http://localhost:3000/libros/Cien Años de Soledad.pdf'
   }
 ]
 
